ad7626b11ba829e2153ccb530b101e9c25abae6e
Jibraan Khan <jibraan@Jibraans-MacBook-Air.local>
2022-02-15 15:08:58 -0500

    Bad Design Finished

--- src/main/java/BadDesign/A.java
+++ src/main/java/BadDesign/A.java
@@ -1,6 +1,16 @@
 package BadDesign;
 
 public class A
-{
-
-}
+{public class B{public String D;public double E;public double F;public String j;public B(String g,double h,double i){this.D=g;this.E=h;this.F=i;}
+public B(String g,double h,double i,String k){this.D=g;this.E=h;this.F=i;this.j=k;}
+public void set(String l){this.j=l;}
+public void set2(double w){this.F=w;}}
+B[]C;int n=0;public A()
+{C=new B[1];}
+public void m(B o){if(n==C.length){B[]new_=new B[C.length*2];for(int i=0;i<C.length;i++){new_[i]=C[i];}
+this.C=new_;}
+C[n]=o;n++;}
+public double[]z(){double[]x=new double[n];for(int i=0;i<n;i++){if(this.C[i].j=="h"){if(C[i].F>40){x[i]=40*C[i].E + ((C[i].F-40)*(1.5*C[i].E));}else{x[i]=C[i].F*C[i].E;}}else if(this.C[i].j=="s"){x[i]=40*C[i].E;}else{x[i]=C[i].F*C[i].E;}}
+return x;}
+public static void main(String[]args)
+{}} 
--- src/test/java/BadDesign/ATest.java
+++ src/test/java/BadDesign/ATest.java
@@ -1,16 +1,39 @@
 package BadDesign;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-class ATest
+public class ATest
 {
+	A v;
+	A.B h;
+	A.B f;
+	A.B m;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		v = new A();
+		h = v.new B("John", 5, 35);
+		f = v.new B("Steve", 12, 59, "c");
+		h.set("s");
+		m = v.new B("Chris", 10, 50);
+		m.set("h");
+		m.set2(45);
+		v.m(h);
+		v.m(f);
+		v.m(m);
+	}
 
 	@Test
-	void test()
+	public void test()
 	{
-		fail("Not yet implemented");
+		double[] a = new double[] {200, 708, 475};
+		
+		assertTrue(Arrays.equals(a, v.z()));
 	}
 
 }
